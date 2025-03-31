fe07033323881da958071ddb956f796904663055
animesh1414 <e22cseu1414@bennett.edu.in>
2025-03-31 21:20:30 +0530

    Chat Page partially created 3:02:45

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotnev from 'dotenv';
 import cookieParser from "cookie-parser";
+import cors from 'cors';
 
 import authRoutes from './routes/auth.route.js';
 import { connectDB } from './lib/db.js';
@@ -13,9 +14,13 @@ const PORT = process.env.PORT
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB();
---
+++ frontend/src/pages/TestPage.jsx
@@ -0,0 +1,11 @@
+const TestPage = () => {
+    console.log("TestPage rendering");
+    return (
+      <div className="bg-red-500 text-white p-10 text-3xl">
+        Test Page!
+      </div>
+    );
+  };
+  
+  export default TestPage;
+  
